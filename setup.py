cda71d9e971f6a5e70d3e41a23090b7e4da7d961
Daniel Pittman <daniel@pittmo.net>
2018-08-04 22:03:12 -0600

    Revert setup.py to allow for Python 2.x, updated .gitignore

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import io
 from setuptools import setup
 
 setup(
@@ -7,7 +8,7 @@ setup(
     url='https://github.com/padelt/temper-python',
     version='1.5.3',
     description='Reads temperature from TEMPerV1 devices (USB 0c45:7401)',
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=io.open('README.md', encoding='utf-8').read(),
     packages=['temperusb'],
     install_requires=[
         'pyusb>=1.0.0rc1',
